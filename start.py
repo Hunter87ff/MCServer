a8a43f32827cf38ddb2d50aac029595b66a07011
hunter87ff <huntergaming702@gmail.com>
2024-09-18 19:11:46 +0530

    update

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import os, threading
 # os.system("wget https://localtonet.com/download/localtonet-linux-x64.zip")
 # os.system("unzip localtonet-linux-x64.zip")
 # os.system("chmod 777 ./localtonet")
-with open("plugins/playiy-gg/config.yml", "w") as f: f.write(f"agent-secret: {os.environ['pggtoken']}") 
+with open("plugins/playit-gg/config.yml", "w") as f: f.write(f"agent-secret: {os.environ['pggtoken']}") 
 # os.system(f"sudo ngrok config add-authtoken {os.environ['ngtoken']}")
 def task1(): os.system("java -Xmx1024M -Xms1024M -jar server.jar nogui")
 def task2(): os.system(f"./localtonet authtoken {os.environ['lttoken']}")
