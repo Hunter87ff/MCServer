9a1ed04f47523d1013aca22e98a0eb9b16c78c98
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-09-18 16:41:44 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import os, threading
+os.system(f"sudo ngrok config add-authtoken {os.environ['ngtoken']}")
 def task1(): os.system("java -Xmx1024M -Xms1024M -jar server.jar nogui")
 def task2(): os.system("ngrok tcp  25565")
 threads = [threading.Thread(target=task1), threading.Thread(target=task2)]
